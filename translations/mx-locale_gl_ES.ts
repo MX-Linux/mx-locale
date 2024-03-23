36d99eec6e1a90d3d09f8e7b57eff1decf738153
Adrian <adrian@mxlinux.org>
2024-03-23 12:53:18 -0400

    New build

--- translations/mx-locale_gl_ES.ts
+++ translations/mx-locale_gl_ES.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ja">
+<TS version="2.1" language="gl_ES">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>Program_Name</source>
-        <translation>プログラム名(_N)</translation>
+        <translation>Nome_do_Programa</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="46"/>
         <source>Quit application</source>
-        <translation>アプリケーションの終了</translation>
+        <translation>Saír do aplicativo</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="49"/>
         <source>Close</source>
-        <translation>閉じる</translation>
+        <translation>Cerrar</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="56"/>
@@ -26,12 +26,12 @@
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>About this application</source>
-        <translation>このアプリケーションについて</translation>
+        <translation>Sobre esta aplicación</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="97"/>
         <source>About...</source>
-        <translation>情報...</translation>
+        <translation>Sobre...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="104"/>
@@ -41,12 +41,12 @@
     <message>
         <location filename="../mainwindow.ui" line="120"/>
         <source>Display help </source>
-        <translation>ヘルプの表示</translation>
+        <translation>Amosar axuda</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="123"/>
         <source>Help</source>
-        <translation>ヘルプ</translation>
+        <translation>Axuda</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="130"/>
@@ -57,84 +57,84 @@
         <location filename="../mainwindow.ui" line="184"/>
         <source>System Locale</source>
         <extracomment> language selection, also default for locale subvariables if not otherwise defined</extracomment>
-        <translation>システムロケール</translation>
+        <translation>Localización do sistema</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="190"/>
         <source>PushButton</source>
-        <translation>プッシュボタン</translation>
+        <translation>PushButton</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="197"/>
         <source>Current System Language:</source>
-        <translation>現在のシステム言語:</translation>
+        <translation>Lingua actual do sistema:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="217"/>
         <source>Set all locale categories to current language</source>
-        <translation>すべてのロケール関係の項目を現在の言語に合わせる</translation>
+        <translation>Establecer todas as categorías de localidade á lingua actual</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="225"/>
         <source>Locale Categories</source>
         <extracomment>defines individual subvariables for locale</extracomment>
-        <translation>ロケールのカテゴリ</translation>
+        <translation>Categorías de localización</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="251"/>
         <source>Numeric</source>
-        <translation>数値</translation>
+        <translation>Numérica</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="265"/>
         <source>Time</source>
-        <translation>時刻</translation>
+        <translation>Horário</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="279"/>
         <source>Collate</source>
-        <translation>順序</translation>
+        <translation>Ordenación</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="293"/>
         <source>Currency</source>
-        <translation>通貨</translation>
+        <translation>Moeda</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="307"/>
         <source>Messages</source>
-        <translation>メッセージ</translation>
+        <translation>Mensaxes</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="321"/>
         <source>Paper Size</source>
-        <translation>用紙の規格</translation>
+        <translation>Tamaño do papel</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="335"/>
         <source>Name Style</source>
-        <translation>氏名</translation>
+        <translation>Estilo de nome</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="349"/>
         <source>Address</source>
-        <translation>アドレス</translation>
+        <translation>Enderezo</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="363"/>
         <source>Telephone</source>
-        <translation>電話番号</translation>
+        <translation>Teléfono</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="377"/>
         <source>Measurement</source>
-        <translation>計量単位</translation>
+        <translation>Medición</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="409"/>
         <source>Locale Management</source>
         <extracomment>Tab to manage generation of locale caches</extracomment>
-        <translation>ロケール管理</translation>
+        <translation>Xestión de localización</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="415"/>
@@ -142,60 +142,60 @@
         <location filename="../mainwindow.cpp" line="338"/>
         <source>All</source>
         <extracomment>elements of filter box</extracomment>
-        <translation>利用可能なロケールの全リスト</translation>
+        <translation>Todos</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="424"/>
         <location filename="../mainwindow.cpp" line="289"/>
         <source>Enabled</source>
-        <translation>有効なロケール（チェックで無効化）</translation>
+        <translation>Activado</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="429"/>
         <location filename="../mainwindow.cpp" line="288"/>
         <source>Disabled</source>
-        <translation>無効なロケール（チェックで有効化）</translation>
+        <translation>Desactivado</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="454"/>
         <source>Disable all locales except the one in use</source>
         <extracomment>disables generation of any locale except on that is currently in use</extracomment>
-        <translation>使用中のロケール以外をすべて無効にする</translation>
+        <translation>Desactiva todas as localizacións excepto a que se está a usar.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="473"/>
         <source>search</source>
-        <translation>検索</translation>
+        <translation>busca</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="483"/>
         <source>Reset locale list to default</source>
-        <translation>ロケールリストを既定にリセット</translation>
+        <translation>Restablecer a lista de configuracións rexionais á predeterminada</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="494"/>
         <source>Changes go into effect on login</source>
-        <translation>変更は次回ログイン時に有効となります</translation>
+        <translation>Os cambios entran en vigor ao iniciar sesión.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="74"/>
         <source>MX Locale</source>
-        <translation>MX ロケールマネージャ</translation>
+        <translation>MX Lingua</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="142"/>
         <source>About %1</source>
-        <translation> %1 について</translation>
+        <translation>Sobre %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="144"/>
         <source>Version: </source>
-        <translation>バージョン：</translation>
+        <translation>Versión: </translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="145"/>
         <source>Program for changing language and locale categories</source>
-        <translation>言語やロケールに関する項目の設定を管理するプログラム</translation>
+        <translation>Programa para cambiar categorías de idioma y configuración regional</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="148"/>
@@ -205,25 +205,25 @@
     <message>
         <location filename="../mainwindow.cpp" line="149"/>
         <source>%1 License</source>
-        <translation>%1 ライセンス</translation>
+        <translation>Licenza de %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="156"/>
         <source>%1 Help</source>
-        <translation>%1 ヘルプ</translation>
+        <translation>Axuda para %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="259"/>
         <source>Locale in use: &lt;b&gt;%1&lt;/b&gt;</source>
         <comment>shows the current system locale, in bold</comment>
-        <translation>現在のロケール: &lt;b&gt;%1&lt;/b&gt;</translation>
+        <translation>Locale en uso: &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="281"/>
         <location filename="../mainwindow.cpp" line="287"/>
         <source>All</source>
         <comment>all as in everything</comment>
-        <translation>すべて</translation>
+        <translation>Todos</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="304"/>
@@ -231,31 +231,31 @@
         <location filename="../mainwindow.cpp" line="355"/>
         <location filename="../mainwindow.cpp" line="361"/>
         <source>Error</source>
-        <translation>エラー</translation>
+        <translation>Erro</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="305"/>
         <source>Can&apos;t disable locale in use</source>
         <comment>message that the chosen locale cannot be disabled because it is in active usage</comment>
-        <translation>使用中のロケールは無効にできません</translation>
+        <translation>Non se pode desactivar a configuración rexional en uso</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="336"/>
         <source>Locales enabled: %1</source>
         <comment>label for a numerical count of enabled and available locales</comment>
-        <translation>有効なロケール: %1</translation>
+        <translation>Locais habilitados: %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="349"/>
         <location filename="../mainwindow.cpp" line="355"/>
         <location filename="../mainwindow.cpp" line="361"/>
         <source>Could not open %1</source>
-        <translation>%1 を開くことができません</translation>
+        <translation>Non se puido abrir %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="389"/>
         <source>Locales enabled: %1</source>
-        <translation>利用可能なロケール: %1</translation>
+        <translation>Locais habilitados: %1</translation>
     </message>
 </context>
 <context>
@@ -263,54 +263,54 @@
     <message>
         <location filename="../about.cpp" line="34"/>
         <source>License</source>
-        <translation>ライセンス</translation>
+        <translation>Licenza</translation>
     </message>
     <message>
         <location filename="../about.cpp" line="35"/>
         <location filename="../about.cpp" line="45"/>
         <source>Changelog</source>
-        <translation>更新履歴</translation>
+        <translation>Rexistro dos cambios</translation>
     </message>
     <message>
         <location filename="../about.cpp" line="36"/>
         <source>Cancel</source>
-        <translation>キャンセル</translation>
+        <translation>Cancelar</translation>
     </message>
     <message>
         <location filename="../about.cpp" line="54"/>
         <source>&amp;Close</source>
-        <translation>閉じる(&amp;C)</translation>
+        <translation>&amp;Cerrar</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="57"/>
         <source>MX Locale is a tool used for managing locale settings in MX Linux</source>
-        <translation>MX ロケールマネージャは、MX Linux 用のロケール管理ツールです</translation>
+        <translation>MX Locale é unha ferramenta utilizada para xestionar a configuración de localización en MX Linux.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="60"/>
         <source>Show only language selection tab.</source>
-        <translation>言語選択タブのみを表示します。</translation>
+        <translation>Mostra só a lapela de selección de idioma.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="61"/>
         <source>Show all locale categories.</source>
-        <translation>すべてのロケール項目を表示します。</translation>
+        <translation>Mostra todas as categorías de localización.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="71"/>
         <location filename="../main.cpp" line="79"/>
         <source>Error</source>
-        <translation>エラー</translation>
+        <translation>Erro</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="72"/>
         <source>You seem to be logged in as root, please log out and log in as normal user to use this program.</source>
-        <translation>root としてログインしているようです。このプログラムを使用するには、一度ログアウトして通常のユーザとしてログインしてください。</translation>
+        <translation>O usuario parece ser root; para usar este programa, pechar a sesión e iniciar sesión como usuario normal.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="80"/>
         <source>You must run this program with admin access.</source>
-        <translation>このプログラムは管理者権限で実行してください。</translation>
+        <translation>Debes executar este programa con acceso de administrador.</translation>
     </message>
 </context>
 <context>
@@ -318,34 +318,34 @@
     <message>
         <location filename="../choosedialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation>ダイアログ</translation>
+        <translation>Diálogo</translation>
     </message>
     <message>
         <location filename="../choosedialog.ui" line="48"/>
         <source>search</source>
-        <translation>検索</translation>
+        <translation>busca</translation>
     </message>
     <message>
         <location filename="../choosedialog.ui" line="58"/>
         <source>If your desired language is not listed here, enable it in &quot;Locale management&quot; tab</source>
-        <translation>希望する言語がここに表示されていない場合は、「ロケール管理」タブで有効にしてください</translation>
+        <translation>Se o teu idioma desexado non aparece aquí, actívalo na lapela &quot;Xestión de localización&quot;</translation>
     </message>
     <message>
         <location filename="../choosedialog.cpp" line="25"/>
         <source>MX Locale</source>
         <comment>name of application</comment>
-        <translation>MX ロケールマネージャ</translation>
+        <translation>MX Lingua</translation>
     </message>
     <message>
         <location filename="../choosedialog.cpp" line="41"/>
         <source>Error</source>
-        <translation>エラー</translation>
+        <translation>Erro</translation>
     </message>
     <message>
         <location filename="../choosedialog.cpp" line="42"/>
         <source>Could not open %1</source>
         <comment>message that a file could not be open, file takes place of %1</comment>
-        <translation>%1 を開くことができません</translation>
+        <translation>Non se puido abrir %1</translation>
     </message>
 </context>
 </TS>
