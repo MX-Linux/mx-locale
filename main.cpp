9e9065320ab475475222adc2ac28b6201a212e38
Adrian <adrian@mxlinux.org>
2025-08-07 15:05:20 -0400

    Set Qt platform to XCB if not set

--- main.cpp
+++ main.cpp
@@ -37,6 +37,13 @@
 
 int main(int argc, char *argv[])
 {
+    // Set Qt platform to XCB (X11) if not already set and we're in X11 environment
+    if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+        if (!qEnvironmentVariableIsEmpty("DISPLAY")) {
+            qputenv("QT_QPA_PLATFORM", "xcb");
+        }
+    }
+
     QApplication a(argc, argv);
 
     if (a.arguments().contains("--version") || a.arguments().contains("-v")) {
