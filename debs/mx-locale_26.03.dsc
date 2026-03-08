facc81344f5b1c9681bc6e9499e009c7b77d8a72
Adrian <adrian@mxlinux.org>
2026-03-07 19:14:11 -0500

    New build

---
+++ debs/mx-locale_26.03.dsc
@@ -0,0 +1,34 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+Format: 3.0 (native)
+Source: mx-locale
+Binary: mx-locale
+Architecture: any
+Version: 26.03
+Maintainer: Dolphin Oracle <dolphinoracle@gmail.com>
+Homepage: https://github.com/MX-Linux/mx-locale
+Standards-Version: 3.9.8
+Vcs-Git: git://github.com/MX-Linux/mx-locale
+Build-Depends: debhelper-compat (= 12), cmake (>= 3.16), ninja-build, qt6-base-dev, qt6-base-dev-tools, qt6-tools-dev, qt6-tools-dev-tools
+Package-List:
+ mx-locale deb admin optional arch=any
+Checksums-Sha1:
+ 580d016d158c8938eec0bc43a8624038783bac88 2477380 mx-locale_26.03.tar.xz
+Checksums-Sha256:
+ f81cc3e82bfaf609742308ab645c501eb69e3fc038927f3b9492c7b3413bd824 2477380 mx-locale_26.03.tar.xz
+Files:
+ e37bbd14b80e10b4256c248c1606eb7b 2477380 mx-locale_26.03.tar.xz
+Original-Maintainer: Adrian <adrian@mxlinux.org>
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCgAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAmmsv0oTHGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumBnYCACgM1tGxU5YxOUPuCzbtfhJb2vpZp55
+hiINhQ4dEDQUfja4bNp8bt51aSfCiB7K+V5kOXU2wgJHxd8EkvYTYT8xSWh2onCq
+tvHH2wwi43BCz31Z3qLDsbJoaodDWXdXIbVOPT33BurGrpYHFUNfSMS9DQfb3hYL
+WCP1N5lMtCyXbw5QQxzBFMMUB4x47u2VfgMMoksxmuS+KOfmKOUE9rnbwWsR9/dH
+HCoeZeJ9aU6x7l3Wl6zIeiBbqnluC3UBU3/qGF3D2ByHrsA0oljP+yhOrE9eL9EN
+MImBGORjGQUCkL718zJcvEQKaq5OmrLb5frs9DlbLgJsTlR8Q6FEL9A3
+=3L+8
+-----END PGP SIGNATURE-----
--- translations/mx-locale_ka.ts
+++ translations/mx-locale_ka.ts
@@ -1,20 +1,20 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="ca">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="ka">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>Program_Name</source>
-        <translation>Nom_programa</translation>
+        <translation>პროგრამის_სახელი</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="46"/>
         <source>Quit application</source>
-        <translation>Surt de l&apos;aplicació</translation>
+        <translation>აპლიკაციიდან გასვლა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="49"/>
         <source>Close</source>
-        <translation>Tanca</translation>
+        <translation>დახურვა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="55"/>
@@ -24,12 +24,12 @@
     <message>
         <location filename="../mainwindow.ui" line="93"/>
         <source>About this application</source>
-        <translation>Quant a aquest programa</translation>
+        <translation>ამ აპლიკაციის შესახებ</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="96"/>
         <source>About...</source>
-        <translation>Quant a...</translation>
+        <translation>შესახებ...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="102"/>
@@ -39,12 +39,12 @@
     <message>
         <location filename="../mainwindow.ui" line="118"/>
         <source>Display help </source>
-        <translation>Mostra l&apos;ajuda</translation>
+        <translation>დახმარების ჩვენება </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="121"/>
         <source>Help</source>
-        <translation>Ajuda </translation>
+        <translation>დახმარება</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="127"/>
@@ -54,85 +54,85 @@
     <message>
         <location filename="../mainwindow.ui" line="214"/>
         <source>PushButton</source>
-        <translation>Polsador</translation>
+        <translation>დააჭირეთ ღილაკს</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="207"/>
         <source>Current System Language:</source>
-        <translation>Idioma actual del Sistema:</translation>
+        <translation>მიმდინარე სისტემის ენა:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="181"/>
         <source>System Language</source>
         <extracomment> language selection, also default for locale subvariables if not otherwise defined</extracomment>
-        <translation>Idioma del sistema</translation>
+        <translation>სისტემის ენა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="187"/>
         <source>Set all locale categories to current language</source>
-        <translation>Aplica l&apos;idioma actual a totes les categories</translation>
+        <translation>ყველა ლოკალის კატეგორიის დაყენება მიმდინარე ენაზე</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="229"/>
         <source>Locale Categories</source>
         <extracomment>defines individual subvariables for locale</extracomment>
-        <translation>Categories de Localització</translation>
+        <translation>ლოკალის კატეგორიები</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="255"/>
         <source>Numeric</source>
-        <translation>Numèric</translation>
+        <translation>რიცხვობრივი</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="269"/>
         <source>Time</source>
-        <translation>Hora</translation>
+        <translation>დრო</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="283"/>
         <source>Collate</source>
-        <translation>Ordenació</translation>
+        <translation>დაშლა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="297"/>
         <source>Currency</source>
-        <translation>Moneda</translation>
+        <translation>ვალუტა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="311"/>
         <source>Messages</source>
-        <translation>Missatges</translation>
+        <translation>შეტყობინებები</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="325"/>
         <source>Paper Size</source>
-        <translation>Mida del paper</translation>
+        <translation>გვერდის ზომა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="339"/>
         <source>Name Style</source>
-        <translation>Estil del nom</translation>
+        <translation>სტილის სახელი</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="353"/>
         <source>Address</source>
-        <translation>Adreça</translation>
+        <translation>მისამართი</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="367"/>
         <source>Telephone</source>
-        <translation>Telèfon</translation>
+        <translation>ტელეფონი</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="381"/>
         <source>Measurement</source>
-        <translation>Mesures</translation>
+        <translation>საზომი ერთეული</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="413"/>
         <source>Locale Management</source>
         <extracomment>Tab to manage generation of locale caches</extracomment>
-        <translation>Gestió de Localització</translation>
+        <translation>ლოკალის მართვა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="465"/>
@@ -140,45 +140,45 @@
         <location filename="../mainwindow.cpp" line="352"/>
         <source>All</source>
         <extracomment>elements of filter box</extracomment>
-        <translation>Tots </translation>
+        <translation>ყველა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="474"/>
         <location filename="../mainwindow.cpp" line="293"/>
         <source>Enabled</source>
-        <translation>Activat</translation>
+        <translation>ჩართულია</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="479"/>
         <location filename="../mainwindow.cpp" line="292"/>
         <source>Disabled</source>
-        <translation>Desactivat</translation>
+        <translation>გათიშულია</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="445"/>
         <source>Disable all locales except the one in use</source>
         <extracomment>disables generation of any locale except on that is currently in use</extracomment>
-        <translation>Desactiva totes les localitzacions menys la que està en ús</translation>
+        <translation>ყველა ლოკალის გამორთვა გამოყენებულის გარეშე</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="428"/>
         <source>search</source>
-        <translation>cerca</translation>
+        <translation>ძებნა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="455"/>
         <source>Reset locale list to default</source>
-        <translation>Restableix la llista de lllengües als valors per omissió</translation>
+        <translation>ლოკალების სიის ნაგულისხმევზე ჩამოყრა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="221"/>
         <source>Remove MX manuals for languages other than system default</source>
-        <translation>Elimina els manuals de MX per idiomes diferents al del sistema</translation>
+        <translation>MX-ის სახელმძღვანელოს წაშლა ენებისთვის, რომლებიც სისტემის ნაგულისხმევი ენისგან განსხვავდება</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="498"/>
         <source>Changes go into effect on login</source>
-        <translation>Els canvis tindran efecte a l&apos;inici de sessió</translation>
+        <translation>ცვლილებები ძალაში სესიაში შესვლისას შევა</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="79"/>
@@ -188,94 +188,94 @@
     <message>
         <location filename="../mainwindow.cpp" line="153"/>
         <source>About %1</source>
-        <translation>Quant a %1</translation>
+        <translation>%1-ის შესახებ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="155"/>
         <source>Version: </source>
-        <translation>Versió: </translation>
+        <translation>ვერსია: </translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="156"/>
         <source>Program for changing language and locale categories</source>
-        <translation>Programa per canviar d&apos;idioma i categoria de localització</translation>
+        <translation>პროგრამა ენისა და ლოკალის კატეგორიების შესაცვლელად</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="160"/>
         <source>Copyright (c) MX Linux</source>
-        <translation>Copyright (c) MX Linux</translation>
+        <translation>(c) MX Linux საავტორო ფულებები დაცულია</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="161"/>
         <source>%1 License</source>
-        <translation>Llicència de %1</translation>
+        <translation>ლიცენზია %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="168"/>
         <source>%1 Help</source>
-        <translation>Ajuda de %1</translation>
+        <translation>%1-ის დახმარება</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="267"/>
         <source>Locale in use: &lt;b&gt;%1&lt;/b&gt;</source>
         <comment>shows the current system locale, in bold</comment>
-        <translation>Localització en ús: &lt;b&gt;%1&lt;/b&gt;</translation>
+        <translation>გამოყენებული ლოკალი: &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="283"/>
         <source>All</source>
         <comment>all as in everything</comment>
-        <translation>Tots </translation>
+        <translation>ყველა</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="318"/>
         <location filename="../mainwindow.cpp" line="375"/>
         <location filename="../mainwindow.cpp" line="390"/>
         <source>Error</source>
-        <translation>Error</translation>
+        <translation>შეცდომა</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="319"/>
         <source>Can&apos;t disable locale in use</source>
         <comment>message that the chosen locale cannot be disabled because it is in active usage</comment>
-        <translation>No puc desactivar la localització en ús</translation>
+        <translation>ლოკალს, რომელიც გამოიყენება, ვერ გამორთავთ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="350"/>
         <source>Locales enabled: %1</source>
         <comment>label for a numerical count of enabled and available locales</comment>
-        <translation>Localitzacions activades: %1</translation>
+        <translation>ჩართული ლოკალები: %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="375"/>
         <location filename="../mainwindow.cpp" line="390"/>
         <source>Could not open %1</source>
-        <translation>No puc obrir %1</translation>
+        <translation>%1-ის გახსნა შეუძლებელია</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="459"/>
         <source>Locales enabled: %1</source>
-        <translation>Localitzacions activades: %1</translation>
+        <translation>ჩართული ლოკალები: %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="468"/>
         <source>Updating locales, please wait</source>
-        <translation>Actualitzant locales, espereu si us plau</translation>
+        <translation>ლოკალების განახლება. მოითმინეთ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="519"/>
         <source>Remove Manuals</source>
-        <translation>Elimina Manuals</translation>
+        <translation>სახელმძღვანელოების წაშლა</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="519"/>
         <source>No manuals to remove.</source>
-        <translation>No hi ha manuals per eliminar.</translation>
+        <translation>წასაშლელი სახელმძღვანელოების გარეშე.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="526"/>
         <source>Removing packages, please wait</source>
-        <translation>Eliminant paquets, espereu si us plau</translation>
+        <translation>მიმდინარეობს პაკეტების წაშლა. მოითმინეთ</translation>
     </message>
 </context>
 <context>
@@ -283,54 +283,54 @@
     <message>
         <location filename="../about.cpp" line="70"/>
         <source>License</source>
-        <translation>Llicència</translation>
+        <translation>ლიცენზია</translation>
     </message>
     <message>
         <location filename="../about.cpp" line="71"/>
         <location filename="../about.cpp" line="81"/>
         <source>Changelog</source>
-        <translation>Registre de canvis</translation>
+        <translation>ცვლილებების ჟურნალი</translation>
     </message>
     <message>
         <location filename="../about.cpp" line="72"/>
         <source>Cancel</source>
-        <translation>Cancel·la</translation>
+        <translation>გაუქმება</translation>
     </message>
     <message>
         <location filename="../about.cpp" line="94"/>
         <source>&amp;Close</source>
-        <translation>Tan&amp;ca </translation>
+        <translation>&amp;დახურვა</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="68"/>
         <source>MX Locale is a tool used for managing locale settings in MX Linux</source>
-        <translation>MX Locale és una eina per gestionar els paràmetres de localització a MX Linux</translation>
+        <translation>MX Locale MX Linux-ში ლოკალების მართვის პროგრამაა</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="71"/>
         <source>Show only language selection tab.</source>
-        <translation>Mostra només la pestanya de selecció d&apos;idioma.</translation>
+        <translation>მხოლოდ ენის არჩევის ჩანართის ჩვენება.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="72"/>
         <source>Show all locale categories.</source>
-        <translation>Mostra totes les categories de localització.</translation>
+        <translation>ყველა ლოკალის კატეგორიის ჩვენება.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="82"/>
         <location filename="../main.cpp" line="90"/>
         <source>Error</source>
-        <translation>Error</translation>
+        <translation>შეცდომა</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="83"/>
         <source>You seem to be logged in as root, please log out and log in as normal user to use this program.</source>
-        <translation>Sembla que esteu connectat com a administrador, si us plau sortiu i connecteu-vos com a usuari normal per a usar aquest programa.</translation>
+        <translation>როგორც ჩანს, root-ით ბრძანდებით შესული. ამ პროგრამის გამოსაყენებლად გადით, და შემოდით ჩვეულებრივი მომხმარებლით.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="91"/>
         <source>You must run this program with admin access.</source>
-        <translation>Cal executar aquest programa com a administrador.</translation>
+        <translation>ეს პროგრამა ადმინისტრატორის წვდომით უნდა გაუშვათ.</translation>
     </message>
 </context>
 <context>
@@ -338,17 +338,17 @@
     <message>
         <location filename="../choosedialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation>Diàleg</translation>
+        <translation>დიალოგი</translation>
     </message>
     <message>
         <location filename="../choosedialog.ui" line="48"/>
         <source>search</source>
-        <translation>cerca</translation>
+        <translation>ძებნა</translation>
     </message>
     <message>
         <location filename="../choosedialog.ui" line="58"/>
         <source>If your desired language is not listed here, enable it in &quot;Locale management&quot; tab</source>
-        <translation>Si l&apos;idioma que voleu no és a la llista, activeu-lo a la pestanya de &quot;Gestió de localitzacions&quot;</translation>
+        <translation>თუ თქვენი სასურველი ენა ამ ჩამონათვალში არაა, ჩართეთ ის ლოკალების მართვის ჩანართში</translation>
     </message>
     <message>
         <location filename="../choosedialog.cpp" line="25"/>
@@ -359,13 +359,13 @@
     <message>
         <location filename="../choosedialog.cpp" line="40"/>
         <source>Error</source>
-        <translation>Error</translation>
+        <translation>შეცდომა</translation>
     </message>
     <message>
         <location filename="../choosedialog.cpp" line="41"/>
         <source>Could not open %1</source>
         <comment>message that a file could not be opened, file takes place of %1</comment>
-        <translation>No puc obrir %1</translation>
+        <translation>%1-ის გახსნა შეუძლებელია</translation>
     </message>
 </context>
 </TS>
